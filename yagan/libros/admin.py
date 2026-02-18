66c084fcae9e1e0914a3ea8e17b05abe064c82ce
xgarridoig-jpg <xgarridoig@gmail.com>
2026-02-18 12:27:01 -0300

    se habilita session en panel

--- yagan/libros/admin.py
+++ yagan/libros/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.sessions.models import Session
+from .models import Libro
 
-# Register your models here.
+admin.site.register(Session)
+admin.site.register(Libro)
